11aab508d9791183828af1d609f5914607ddd200
shiyuwudi <289052669@qq.com>
2016-11-28 00:31:01 +0800

    把所有用到的变量都转移到redux store中

--- js/reducer.js
+++ js/reducer.js
@@ -8,19 +8,32 @@ import myCombineReducers from './myCombineReducers'
 const combinedReducer = myCombineReducers({ movies, actors});
 export const store = createStore(combinedReducer);
 
-function movies(state = [], action) {
+function movies(state = {
+    subjects: [],
+    listLoading: false,
+}, action) {
 
     switch (action.type) {
 
         case "IN_THEATERS":
             //正在上映
-            return action.data.subjects;
+            return {
+                ...state,
+                subjects: action.data.subjects,
+                listLoading: false,
+            };
+
+        case "SET_LISTLOADING":
+            return {
+                ...state,
+                listLoading: action.listLoading,
+            };
 
         default:
             return state;
     }
 
-};
+}
 
 function actors(state = {}, action) {
 
