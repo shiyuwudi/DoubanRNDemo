c01c111065b00707b155365f4d40981d48c2da0b
shiyuwudi <289052669@qq.com>
2016-11-28 00:42:03 +0800

    REDUX_DEVTOOLS

--- js/reducer.js
+++ js/reducer.js
@@ -6,7 +6,7 @@ import { createStore } from 'redux'
 import myCombineReducers from './myCombineReducers'
 
 const combinedReducer = myCombineReducers({ movies, actors});
-export const store = createStore(combinedReducer);
+export const store = createStore(combinedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 function movies(state = {
     subjects: [],
