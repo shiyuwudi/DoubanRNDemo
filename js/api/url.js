a03acb8e77accad137184bd12a480a1c707f90c4
shiyuwudi <289052669@qq.com>
2016-11-27 13:08:11 +0800

    影片详情url

--- js/api/url.js
+++ js/api/url.js
@@ -1,3 +1,4 @@
 export default {
-  in_theaters: "https://api.douban.com/v2/movie/in_theaters"
+  in_theaters: "https://api.douban.com/v2/movie/in_theaters",
+  subject_detail: "https://api.douban.com/v2/movie/subject/26630781"
 }
